8ec17466391e5f9f2f3c1521ffb3f0e670aadfcc
wassim chikhi <chikhimedwassim@gmail.com>
2024-05-23 16:26:18 +0200

    finished scrolling while searching + handling DataBase deployment

--- test/SmartOpDashboard/src/app/app.component.ts
+++ test/SmartOpDashboard/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { LocalDataSource } from 'ng2-smart-table';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'SmartOpDashboard';
-  data: any[] = [];
+  data: LocalDataSource = new LocalDataSource();
   settings = {
     columns: {
       chirurgien: { title: 'Chirurgien' },
@@ -32,17 +33,34 @@ export class AppComponent {
   page: number = 1;
   limit: number = 10;
   isLoading: boolean = false;
+  isSearching: boolean = false;
 
   constructor(private http: HttpClient) {
     this.loadChirurgiens();
+    this.data.onChanged().subscribe((change) => {
+      if (change.action === 'filter') {
+        this.isSearching = change.filter.filters.some((filter: any) => filter.search !== '');
+      } else {
+        this.isSearching = false;
+      }
+    });
   }
 
   loadChirurgiens() {
     if (this.isLoading) return;
     this.isLoading = true;
-    this.http.get<any[]>(`http://localhost:3000/api/interventions/chirurgiens?page=${this.page}&limit=${this.limit}`)
+    const params = new HttpParams()
+      .set('page', this.page.toString())
+      .set('limit', this.limit.toString());
+
+    this.http.get<any[]>('http://localhost:3000/api/interventions/chirurgiens', { params })
       .subscribe(newData => {
-        this.data = [...this.data, ...newData];
+        if (this.page === 1) {
+          this.data.load(newData);
+        } else {
+          newData.forEach(item => this.data.add(item));
+          this.data.refresh();
+        }
         this.page++;
         this.isLoading = false;
       }, error => {
@@ -52,11 +70,15 @@ export class AppComponent {
   }
 
   @HostListener('window:scroll', ['$event'])
-  onWindowScroll() {
+  onScroll(event: any) {
+    if (this.isSearching || this.isLoading) {
+      return; // Do not load more data if a search is active or if data is already loading
+    }
+
     const threshold = 150;
     const position = window.scrollY + window.innerHeight;
     const height = document.body.scrollHeight;
-    if (position > height - threshold && !this.isLoading) {
+    if (position > height - threshold) {
       this.loadChirurgiens();
     }
   }
